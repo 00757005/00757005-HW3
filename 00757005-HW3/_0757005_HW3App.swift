b4c5e2b2164f33af2d2e94106e765cc2ffa98e3d
andy <andy870622@gmail.com>
2020-10-29 15:31:50 +0800

    Initial Commit

---
+++ 00757005-HW3/_0757005_HW3App.swift
@@ -0,0 +1,17 @@
+//
+//  _0757005_HW3App.swift
+//  00757005-HW3
+//
+//  Created by User15 on 2020/10/29.
+//
+
+import SwiftUI
+
+@main
+struct _0757005_HW3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
